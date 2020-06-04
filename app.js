63c92adc55bf4351f2982d2b52fb25de0a6b6ff5
Margui <margui.berjon@gmail.com>
2020-06-04 11:47:15 +0200

    feature: validated selected currency

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const form = document.querySelector('.js-form');
 
+
+
 form.addEventListener('submit', quote)
 
 function quote(ev) {
   ev.preventDefault();
-  console.log('en');
+  const currencySelect = document.querySelector('.js-currency');
+  const selectedcurrency = currencySelect.options[currencySelect.selectedIndex].value
 
+  const crytoCurrencySelect = document.querySelector('.js-cryptocurrency');
+  const selectedcrytoCurrency = crytoCurrencySelect.options[crytoCurrencySelect.selectedIndex].value
+
+  if (selectedcurrency === '' || selectedcrytoCurrency === '') {
+    console.log('completa');
+  } else {
+    console.log('bien');
+
+  }
 
 }
